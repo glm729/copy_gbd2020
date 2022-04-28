1701d0fb8b8fd61ad99d38ed3a5a2adc85289f62
George L. Malone <George.Malone@telethonkids.org.au>
2022-04-28 08:01:29 +0800

    Additional constructors and Base.string overload
    * Add section headers
    
    * Add constructors for VariableGBDFilename and V20Filename for using
      BaseGBDFilename and BaseV20Filename
    
    * Add Base.string overload for VariableGBDFilename
    
    TODO: Add Base.string overload for V20Filename

--- src/types.jl
+++ src/types.jl
@@ -1,6 +1,14 @@
+# Abstract types
+# -----------------------------------------------------------------------------
+
+
 abstract type AbstractGBDFilename end
 abstract type AbstractVariableGBDFilename <: AbstractGBDFilename end
 abstract type AbstractV20Filename <: AbstractGBDFilename end
+
+
+# Structs
+# -----------------------------------------------------------------------------
 
 
 """
@@ -29,6 +37,10 @@ struct VariableGBDFilename{T <: AbstractString} <: AbstractVariableGBDFilename
 
     function VariableGBDFilename(dir::T, date::T, var::T, suffix::T) where {T}
         new{T}(dir, date, var, suffix)
+    end
+
+    function VariableGBDFilename(base::BaseGBDFilename, var::T) where {T}
+        new{T}(base.dir, base.date, var, base.suffix)
     end
 
 end
@@ -81,4 +93,39 @@ struct V20Filename{T <: AbstractString} <: AbstractV20Filename
         new{T}(dir, date, subdir, prefix, suffix, year, month)
     end
 
+    function V20Filename(base::BaseV20Filename, year::T, month::T) where {T}
+        new{T}(
+            base.dir,
+            base.date,
+            base.subdir,
+            base.prefix,
+            base.suffix,
+            year,
+            month)
+    end
+
 end
+
+
+# Method implementations
+# -----------------------------------------------------------------------------
+
+
+"""
+    Base.string(x::VariableGBDFilename)::String
+
+Method overload for `Base.string`, to build a string from a
+`VariableGBDFilename` object.
+
+Joins `var` and `suffix` without a separator, and joins the complete path
+separated by a single solidus.
+"""
+function Base.string(x::VariableGBDFilename)::String
+    join([x.dir, x.date, "$(x.var)$(x.suffix)"], "/")
+end
+
+
+# """
+# """
+# function Base.string(x::V20Filename)
+# end
