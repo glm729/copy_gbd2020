24aa78a673ac4c1d5278f8b1cf5213087a62b968
George L. Malone <George.Malone@telethonkids.org.au>
2022-05-02 13:59:19 +0800

    Test finding and converting ITN path data
    * Remove printing spec
    
    * Add print line to show testing stage:
      * V20 or ITN
    
    * Add testing for `find_intervention` and `make_intervention_new`:
      * ITN

--- src/main.jl
+++ src/main.jl
@@ -12,6 +12,7 @@ using JSON
 # -----------------------------------------------------------------------------
 
 
+include("find_intervention.jl")
 include("find_v20.jl")
 include("read_spec.jl")
 include("regex_escape.jl")
@@ -37,14 +38,17 @@ function main()
 
     spec = read_spec(ARGS[1])
 
-    # println(JSON.print(spec, 4))
-
+    println("\033[36mV20\033[m")
     v20_old = find_v20(spec["v20"]["from"])
-
     v20_new = make_v20_new(spec["v20"]["to"], v20_old[1])
-
     println(string(v20_old[1]))
     println(string(v20_new))
+
+    println("\033[36mITN\033[m")
+    itn_old = find_intervention(spec["itn"]["from"])
+    itn_new = make_intervention_new(spec["itn"]["to"], itn_old[2])
+    println(string(itn_old[2]))
+    println(string(itn_new))
 
 end
 
